2be3efcab7e8cc3a6048f05928cb245e79be5877
JoseLRiano <joselriano@yahoo.com>
2020-05-22 05:31:00 -0400

    Updates

---
+++ precache-manifest.7fb3eb4dddbc723e586abcb73fa45cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ec16f2cf3b101ca319b32cadce28b7",
+    "url": "/joselriano.github.io/BoxMaker/index.html"
+  },
+  {
+    "revision": "0eb42024ac7e60fa358e",
+    "url": "/joselriano.github.io/BoxMaker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "08d765512d5a11f1ecf6",
+    "url": "/joselriano.github.io/BoxMaker/static/js/2.c858e40d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/joselriano.github.io/BoxMaker/static/js/2.c858e40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb42024ac7e60fa358e",
+    "url": "/joselriano.github.io/BoxMaker/static/js/main.38517a8f.chunk.js"
+  },
+  {
+    "revision": "3adad9f745dc80db2a13",
+    "url": "/joselriano.github.io/BoxMaker/static/js/runtime-main.36fb1c8f.js"
+  }
+]);
